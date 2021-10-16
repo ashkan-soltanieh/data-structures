39d3eeb5fe13281f37e7f0ec70c99e6238fce9d6
Ashkan Soltanieh <ashkan98@gmail.com>
2021-10-16 11:33:49 -0600

    Implement stacks using linkedlist

--- src/com/ashkansoltanieh/stack/Stack.java
+++ src/com/ashkansoltanieh/stack/Stack.java
@@ -1,41 +1,33 @@
 package com.ashkansoltanieh.stack;
 
+
 import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.LinkedList;
 
 public class Stack<T> {
-    private int arraySize = 5;
-    private double arraySizeIncrementer = 1.5;
-    private T[] items = (T[])(new Object[arraySize]);
-    private int count = 0;
+    private LinkedList<T> items = new LinkedList<>();
 
     public void push(T item) {
-        if (item == null) throw new StackOverflowError();
-        if(count == arraySize) {
-            arraySize *= arraySizeIncrementer;
-            items = Arrays.copyOfRange(items, 0, arraySize);
-        }
-        items[count++] = item;
+        items.addLast(item);
     }
 
     public T pop() {
-        if (count == 0) throw new EmptyStackException();
-        var top = items[--count];
-        return top;
+        return items.removeLast();
     }
 
     public T peek() {
-        if (count == 0) throw new EmptyStackException();
-        return items[count - 1];
+        return items.getLast();
     }
 
     public boolean isEmpty() {
-        return count == 0;
+       return items.isEmpty();
     }
 
     @Override
     public String toString() {
-        var content = Arrays.copyOfRange(items, 0, count);
-        return Arrays.toString(content);
+        return "Stack{" +
+                "items=" + items +
+                '}';
     }
 }
